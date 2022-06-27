5e880b057c307d3e5e442b216cedc5e6699cfcb6
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-06-27 08:55:03 -0300

    teste

---
+++ site/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className='l-1'>
+
+      <h1>
+        Tabela Herói
+      </h1>
+
+    <div className='td-1'>
+      <table>
+        <tr>
+          <th>NOME</th>
+          <th>VOA</th>
+          <th></th>
+        </tr>
+
+        <tr>
+          <td></td>
+
+        </tr>
+      </table>
+    </div>  
+  
+    </main>
+  );
+}
+
+export default App;
